8720ba9a73e50763dd9afd41579e31a5c30ef197
chike <chike@sfc.wide.ad.jp>
2017-05-12 11:54:19 +0900

    K-means clustering by words and output csv file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import pandas as ps
 from bs4 import BeautifulSoup
+import MeCab
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
 
 # intractive shell
 from IPython import embed
@@ -20,6 +24,65 @@ def scraping():
     dataflame = ps.DataFrame(datas,columns=header)
     return dataflame
 
+def parse(unicode_string):
+    tagger = MeCab.Tagger("-Ochasen")
+    text = unicode_string.encode("utf-8")
+    node = tagger.parseToNode(text)
+
+    words = []
+    nouns = []
+    verbs = []
+    adjs = []
+    while node:
+        pos = node.feature.split(",")[0]
+        # unicode 型に戻す
+        word = node.surface.decode("utf-8")
+        if pos == "名詞":
+            nouns.append(word)
+        elif pos == "動詞":
+            verbs.append(word)
+        elif pos == "形容詞":
+            adjs.append(word)
+        words.append(word)
+        node = node.next
+    parsed_words_dict = {
+        "all": words[1:-1],
+        "nouns": nouns,
+        "verbs": verbs,
+        "adjs": adjs
+        }
+    return parsed_words_dict
+
+
+def remove_duplicates(x):
+    y=[]
+    for i in x:
+        if i not in y:
+            y.append(i)
+    return y
+
+
+
 if __name__ == '__main__':
     data = scraping()
-    embed()
+    print("done scraping")
+    stdwordlists = []
+    allwordlist = []
+    for a in range(0,len(data)):
+        words = parse(data.ans[a])
+        stdwordlists.append(words["nouns"])
+        allwordlist.extend(words["nouns"])
+    allwordlist = remove_duplicates(allwordlist)
+    stdstatuses = []
+    for a in range(0,len(stdwordlists)):
+        stdstatus = [0] * len(allwordlist)
+        for word in stdwordlists[a]:
+            i = allwordlist.index(word)
+            stdstatus[i] += 1
+        stdstatuses.append(stdstatus)
+    status = list(stdstatuses)
+    print("k-means clustering")
+    pred = KMeans(n_clusters=15).fit_predict(status)
+
+    data["cluster"] = pred
+    data.to_csv("./datawithcluster.csv")
